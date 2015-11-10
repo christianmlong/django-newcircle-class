e927ba46cbbc48afa8a9a00a20a57a852d3a659a
Christian Long <chlong2@cisco.com>
2015-11-10 12:43:36 -0600

    admin

--- mysite/rest/admin.py
+++ mysite/rest/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
-# Register your models here.
+from models import Question, Answer
+
+class QuestionAdmin(admin.ModelAdmin):
+    pass
+
+class AnswerAdmin(admin.ModelAdmin):
+    pass
+
+admin.site.register(Question, QuestionAdmin)
+admin.site.register(Answer, AnswerAdmin)
+
